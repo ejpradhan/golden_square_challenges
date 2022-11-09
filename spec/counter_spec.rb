a3e072ca373a22525e6d5977038716de4998c5a4
Eva Pradhan <ejpradhan@yahoo.com>
2022-11-09 23:40:33 +0000

    Test complete

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,20 +1,28 @@
 require "counter"
 
 RSpec.describe Counter do
-  it "counts to 2" do
+  it "initially reports a count of zero" do
+    counter = Counter.new
+    expect(counter.report).to eq "Counted to 0 so far."
+  end
+
+  it "count to the the added value with one addition being present" do
     counter = Counter.new
     counter.add(2)
-    result = counter.report
-    expect(result).to eq "Counted to 2 so far."
+    expect(counter.report).to eq "Counted to 2 so far."
+  end
+
+  it "count to the the added value with multiple addition being present" do
+    counter = Counter.new
+    counter.add(2)
+    counter.add(3)
+    counter.add(5)
+    expect(counter.report).to eq "Counted to 10 so far."
   end
 end
 
 # require "counter"
 
 # RSpec.describe Counter do
-#   it "counts to 0" do
-#     counter = Counter.new
-#     result = counter.report
-#     expect(counter.report).to eq "Counted to 0 so far."
-#   end
+#
 # end
--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -10,4 +10,36 @@ RSpec.describe StringBuilder do
     string_builder = StringBuilder.new
     expect(string_builder.size).to eq 0
   end
+
+  context "given one addition of a string" do
+    it "outputs that string" do
+      string_builder = StringBuilder.new
+      string_builder.add("hello")
+      expect(string_builder.output).to eq "hello"
+    end
+
+    it "has a size of that string's length" do
+      string_builder = StringBuilder.new
+      string_builder.add("hello")
+      expect(string_builder.size).to eq 5
+    end
+  end
+
+  context "given many addition of a string" do
+    it "outputs that string" do
+      string_builder = StringBuilder.new
+      string_builder.add("hello")
+      string_builder.add("world")
+      string_builder.add("sister")
+      expect(string_builder.output).to eq "helloworldsister"
+    end
+
+    it "has a size of that string's length" do
+      string_builder = StringBuilder.new
+      string_builder.add("hello")
+      string_builder.add("world")
+      string_builder.add("sister")
+      expect(string_builder.size).to eq 16
+    end
+  end
 end
