89027579baf8a424b255136f065a6a71bd6062be
Eva Pradhan <ejpradhan@yahoo.com>
2022-11-10 10:12:32 +0000

    Reminder update w error testing

--- Phase_One_Testing_Bites/lib/reminder.rb
+++ Phase_One_Testing_Bites/lib/reminder.rb
@@ -1,3 +1,24 @@
+# Reminder  class testin v.1
+
+# class Reminder
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def remind_me_to(task)
+#     @task = task
+#   end
+
+#   def remind()
+#     return "#{@task}, #{@name}!"
+#   end
+# end
+
+# reminder = Reminder.new("Kay")
+# reminder.remind_me_to("Walk the dog")
+# puts reminder.remind()
+
+# Testing errors
 class Reminder
   def initialize(name)
     @name = name
@@ -8,10 +29,17 @@ class Reminder
   end
 
   def remind()
+    # Look here! We want to fail if there is no reminder yet.
+    fail "No reminder set!" if @task.nil?
     return "#{@task}, #{@name}!"
   end
 end
 
-reminder = Reminder.new("Kay")
-reminder.remind_me_to("Walk the dog")
-puts reminder.remind()
+# reminder = Reminder.new("Kay")
+# reminder.remind
+# no reminder set --> No reminder set!
+
+# reminder = Reminder.new("Kay")
+# reminder.remind_me_to("Cook")
+# p reminder.remind
+#  if we set a task /reminder set --> "Cook, Kay!"
--- Phase_One_Testing_Bites/spec/reminder_spec.rb
+++ Phase_One_Testing_Bites/spec/reminder_spec.rb
@@ -1,10 +1,25 @@
+# Reminder class testing
+
+# require "reminder"
+
+# RSpec.describe Reminder do
+#   it "reminds the user to perform a task" do
+#     reminder = Reminder.new("Kay")
+#     reminder.remind_me_to("Walk the dog")
+#     result = reminder.remind()
+#     expect(result).to eq "Walk the dog, Kay!"
+#   end
+# end
+
+# Error testing
+
 require "reminder"
 
 RSpec.describe Reminder do
-  it "reminds the user to perform a task" do
-    reminder = Reminder.new("Kay")
-    reminder.remind_me_to("Walk the dog")
-    result = reminder.remind()
-    expect(result).to eq "Walk the dog, Kay!"
+  context "when no task is set" do
+    it "fails" do
+      reminder = Reminder.new("Kay")
+      expect { reminder.remind() }.to raise_error "No reminder set!"
+    end
   end
 end
