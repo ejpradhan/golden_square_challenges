216a49b6d0f67ea9385584430bca90f884123189
Eva Pradhan <ejpradhan@yahoo.com>
2022-11-10 01:50:36 +0000

    Gratitude class test complete

--- Phase_One_Testing_Bites/spec/gratitudes_spec.rb
+++ Phase_One_Testing_Bites/spec/gratitudes_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Gratitudes do
     gratitudes = Gratitudes.new
     expect(gratitudes.format).to eq "Be grateful for: "
   end
+
+  it "returns one gratitude" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("health")
+    expect(gratitudes.format).to eq "Be grateful for: health"
+  end
+
+  it "returns multiple gratitudes" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("health")
+    gratitudes.add("joy")
+    gratitudes.add("sunshine")
+    expect(gratitudes.format).to eq "Be grateful for: health, joy, sunshine"
+  end
 end
